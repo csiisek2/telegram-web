f2c84d5e5d34c60ac89882b51696de0e959d5005
이승재 <leeseja@iseungjaeui-MacBookAir.local>
2026-01-03 01:13:42 +0900

    Add scroll to top on board page navigation

--- src/pages/FreeBoardPage.jsx
+++ src/pages/FreeBoardPage.jsx
@@ -1,8 +1,12 @@
-
-import React from 'react';
+```
+import React, { useEffect } from 'react';
 import Board from '../components/Board';
 
 const FreeBoardPage = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div style={{ marginTop: '20px' }}>
             <Board boardTitle="자유게시판" boardCategory="자유" preview={false} writeUrl="/free/write" />
--- src/pages/ScammerBoardPage.jsx
+++ src/pages/ScammerBoardPage.jsx
@@ -1,8 +1,12 @@
-
-import React from 'react';
+```
+import React, { useEffect } from 'react';
 import Board from '../components/Board';
 
 const ScammerBoardPage = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div style={{ marginTop: '20px' }}>
             <Board boardTitle="사기꾼 게시판" boardCategory="사기" preview={false} writeUrl="/scammer/write" />
